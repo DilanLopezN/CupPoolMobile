fc2d01a7b96a92970217d5b126d46f1293bb3ea0
Dilan Lopez <dilanlopez009@gmail.com>
2022-11-01 12:53:30 -0300

    add native base configs

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+
+import { NativeBaseProvider, Text, Center } from 'native-base'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NativeBaseProvider>
+      <Center flex={1} bgColor="fuchsia.400">
+        <Text color="black" fontSize={24}>
+          Nlw Cup Poll
+        </Text>
+        <StatusBar style="auto" />
+      </Center>
+    </NativeBaseProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
