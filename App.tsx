9a88450e33832bef8529870d81b4d16e4bd67bd7
Dilan Lopez <dilanlopez009@gmail.com>
2022-11-01 13:06:35 -0300

    own themes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar'
-
 import { NativeBaseProvider, Text, Center } from 'native-base'
+import { THEME } from './src/styles/theme'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Center flex={1} bgColor="fuchsia.400">
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1} bgColor="gray.900">
         <Text color="black" fontSize={24}>
           Nlw Cup Poll
         </Text>
