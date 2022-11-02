f09e590c02aec75fdacd6d0faba74f8c16129fe2
Dilan Lopez <dilanlopez009@gmail.com>
2022-11-02 17:42:21 -0300

    add auth configs with Google

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react'
+import { createContext, ReactNode, useState, useEffect } from 'react'
 import * as Google from 'expo-auth-session/providers/google'
 import * as AuthSession from 'expo-auth-session'
 import * as WebBrowser from 'expo-web-browser'
@@ -21,7 +21,10 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps)
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<UserProps>({} as UserProps)
+
   const [isUserLoading, setIsUserLoading] = useState(false)
+
   const [request, response, promptAsync] = Google.useAuthRequest({
     clientId:
       '509645376300-bsit1ci3of714t5vp36fvitsm2su5jaa.apps.googleusercontent.com',
@@ -41,15 +44,22 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
     }
   }
 
+  async function signInWithGoogle(acess_token: string) {
+    console.log('TOKEN DE AUTENTICAÇÃO ===>', acess_token)
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken)
+    }
+  }, [response])
+
   return (
     <AuthContext.Provider
       value={{
         signIn,
         isUserLoading,
-        user: {
-          name: 'Dilan lopez',
-          avatarUrl: 'https://github.com/DilanLopezN.png'
-        }
+        user
       }}
     >
       {children}
